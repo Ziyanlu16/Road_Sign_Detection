3b799f3409f5babebd36d933cf020781545dd5cc
c22090044 <LuZ29@cardiff.ac.uk>
2023-08-24 16:28:01 +0100

    main

--- contentScript.js
+++ contentScript.js
@@ -1,37 +1,15 @@
-let session;
+// let session;
 
-// Load the ONNX model
-async function loadModel() {
-    session = new onnx.InferenceSession();
-    await session.loadModel('yolov5/yolov5s.onnx');
-}
+// // Load the ONNX model
+// async function loadModel() {
+//     session = new onnx.InferenceSession();
+//     await session.loadModel('yolov5/yolov5s.onnx');
+// }
 
-loadModel();
+// loadModel();
 
 chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
     if(request.message === "start_detection") {
-        // Capture the image from Google Maps Street View.
-        let imageTensor = captureImage();
-        
-        // Run the ONNX.js model for detection
-        let outputMap = await session.run([imageTensor]);
-        
-        // Process the output
-        let detections = processOutput(outputMap);
-        
-        // Overlay the results on the Google Maps image
-        overlayResults(detections);
+        document.body.style.backgroundColor = "black";
     }
 });
-
-function captureImage(){
-
-}
-
-function processOutput(){
-
-}
-
-function overlayResults(){
-
-}
