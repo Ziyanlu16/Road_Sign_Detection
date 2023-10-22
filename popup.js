355da62f674dc7dc2392422f4e251031b4e708a5
Ziyanlu16 <caixukunwuebuzuozhu@gmail.com>
2023-10-22 21:06:27 +0800

    main

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('startDetection').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, {"message": "start_detection"});
-    });
-  });
-  
